d2b7f0be30dc02496f6c4717b293c2b85e70964e
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-01-30 13:18:26 -0500

    created buttons to run applications and open files, preparing to write functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # app_opener
 a python GUI app to open your applications
+
+
+The purpose of this application is to being connecting GUI applications with pyy
--- app.py
+++ app.py
@@ -4,11 +4,30 @@ from tkinter import filedialog, Text
  # allows running apps
 import os
 
+
+#function to run apps
+
+
  # body structure of app
 root = tk.Tk()
 
-#add canvas to make it larger
+#add canvas to make it larger and add color
 
-canvas = tk.Canvas()
+canvas = tk.Canvas(root, height=700, width=700, bg="#f7007b")
+canvas.pack()
+
+# add inside container
+frame = tk.Frame(root, bg="#08e1d7")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+#add button
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, 
+fg="black", bg="#08e1d7")
+openFile.pack()
+#add button
+runApp = tk.Button(root, text="Run Applications", padx=10, pady=5, 
+fg="black", bg="#08e1d7") 
+
+runApp.pack()
 # run gui
 root.mainloop()
