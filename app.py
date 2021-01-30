d5c9ebf155b2cde4172cbb6c8d529affda04743b
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-01-30 12:52:49 -0500

    small gui appears

---
+++ app.py
@@ -0,0 +1,14 @@
+ # allows run GUI
+import tkinter as tk
+from tkinter import filedialog, Text 
+ # allows running apps
+import os
+
+ # body structure of app
+root = tk.Tk()
+
+#add canvas to make it larger
+
+canvas = tk.Canvas()
+# run gui
+root.mainloop()
