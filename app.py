268ae32cf82f3af6653dd5e060205d251d5672cb
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-01-31 10:48:24 -0500

    opens and runs apps

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ apps = []
 
 #function to open apps
 def addApp():  
+    #to delete app acess to whats in frame
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename = filedialog.askopenfile(initialdir="/", title="Select file",
     filetypes=(("executables","*.exe"), ("all files", "*.*")))
 
@@ -19,9 +23,13 @@ def addApp():
     print(filename)
 #create lable
     for app in apps:
-        label = tk.label(frame, text=app, bg="gray")
+        label = tk.Label(frame, text=app, bg="gray")
         label.pack()
-
+# to run the applications
+#loop over apps and start application
+def runApps():
+    for app in apps:
+        os.startfile(app)
 #add canvas to make it larger and add color
 canvas = tk.Canvas(root, height=700, width=700, bg="#f7007b")
 canvas.pack()
@@ -37,7 +45,7 @@ fg="black", bg="#08e1d7", command=addApp)
 openFile.pack()
 #add button
 runApp = tk.Button(root, text="Run Applications", padx=10, pady=5, 
-fg="black", bg="#08e1d7") 
+fg="black", bg="#08e1d7" command=runApps ) 
 
 runApp.pack()
 # run gui
