4245c776de62929fda4f2dc92657c4520a779eea
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-01-30 19:21:59 -0500

    can see all files but can't grab them. also having an error with label will look at when its not late

--- app.py
+++ app.py
@@ -4,16 +4,25 @@ from tkinter import filedialog, Text
  # allows running apps
 import os
 
+ # body structure of app
+root = tk.Tk()
+#empty to add appended apps to
+apps = []
 
-#function to run apps
+#function to open apps
 def addApp():  
     filename = filedialog.askopenfile(initialdir="/", title="Select file",
     filetypes=(("executables","*.exe"), ("all files", "*.*")))
- # body structure of app
-root = tk.Tk()
+
+# append to apps
+    apps.append(filename)
+    print(filename)
+#create lable
+    for app in apps:
+        label = tk.label(frame, text=app, bg="gray")
+        label.pack()
 
 #add canvas to make it larger and add color
-
 canvas = tk.Canvas(root, height=700, width=700, bg="#f7007b")
 canvas.pack()
 
